1531975c8a3a9d532c2f4c7ee46e4f78ea584217
Met486 <54407289+Met486@users.noreply.github.com>
2019-10-10 16:05:45 +0900

    sometimes

--- gitbush.py
+++ gitbush.py
@@ -1,4 +1,19 @@
-def hello():
+import time
+
+
+def hello():    
+    import datetime
+    t1 = time.time()
+    nTime = datetime.datetime.now()
+    print("名前を入力してください")
+    str = input()
+    if(nTime.hour<12):
+        print("おはようございます "+str)
+    elif(nTime.hour>12 and nTime.hour <18):
+        print("こんにちは "+str)
+    else :
+        print("こんばんわ "+str)
+        
     pass
     
 def wait():
@@ -6,4 +21,5 @@ def wait():
     
 def goodbye():
     pass
-    
+
+hello()
