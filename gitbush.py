ff32b6a10c5c22c26c7c80f3499205b62f1ffaf8
Yum-git <yzk.yuma@gmail.com>
2019-10-10 15:48:34 +0900

    second python

--- gitbush.py
+++ gitbush.py
@@ -1,9 +1,23 @@
+import time
+import sys
+
 def hello():
-    pass
+    return time.time()
     
 def wait():
     pass
     
 def goodbye():
-    pass
+    t2 = time.time()
+    print('Goodbye everyone')
+    print('経過時間:')
+    print(str(t2 - t1) + '秒')
+    sys.exit()
+
+t1 = hello()
+
+
+
+    
+  
     
